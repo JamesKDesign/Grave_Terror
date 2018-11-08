d318c94c308900471218789d1fd3239272d7b226
Fishingpilot <warmbaconsalad@gmail.com>
2018-11-08 15:02:04 +1100

    EnemyAttack has been merged into Enemy for ease of use. Enemies can now attack players with delay and rate

--- Grave_Terror/Assets/Scripts/Dennis/Enemy.cs
+++ Grave_Terror/Assets/Scripts/Dennis/Enemy.cs
@@ -31,21 +31,29 @@ public class Enemy : MonoBehaviour
 
 	//Is this actor directly engaging the target?
 	//[HideInInspector]
-	public bool engaging = false;
-	private BaseBehaviour engager;
 
 	//Values to be set in the editor
 	[Tooltip("Actor attack damage")]
 	public int attackDamage;
-	[Tooltip("Actor attack range (in unity meters)")]
-	public float attackRange;
-	[Tooltip("Actor attack rate (delay in seconds)")]
+	[Tooltip("How long the actor takes to wind up his attack (in unity meters)")]
+	public float attackDelay;
+	[Tooltip("Time until next attack can be done after finishing an attack (delay in seconds)")]
 	public float attackRate;
 	[Tooltip("Actor travel speed")]
 	public float movementSpeed;
 
 	[HideInInspector]
+	public bool engaging = false;
+
+	//Deny the actor from moving and turning when he is attacking
+	private bool attackLocked = false;
+	private float attackTimer = 0.0f;
+	private EnemyAttackBox attackHitbox = null;
+
+	[HideInInspector]
 	public float fireDamageOverTime = 0.0f;
+	[HideInInspector]
+	public float fireTime = 0.0f;
 
 	//Public so the controller can easily access it
 	[HideInInspector]
@@ -70,13 +78,13 @@ public class Enemy : MonoBehaviour
 	{
 		behaviours.Add(new EnemyPathing(this, pathingWeight));
 		behaviours.Add(new EnemyFlocking(this, flockingWeight));
-		behaviours.Add(new EnemyWander(this, wanderWeight));
-		//Moved to its own section
-		engager = new EnemyAttack(this, 1.0f); //Weight does not effect this behaviour
+		behaviours.Add(new EnemyWander(this, wanderWeight)); //This one needs work
 
 		cc = GetComponent<CharacterController>();
 
 		health = GetComponent<EnemyHealth>();
+
+		attackHitbox = transform.Find("AttackBox").GetComponent<EnemyAttackBox>();
 	}
 	
 	// Update is called once per frame
@@ -91,17 +99,35 @@ public class Enemy : MonoBehaviour
 			case E_STATE.IGNITED:
 				//Mabye display some fire effect here?
 				health.DamageHealth(fireDamageOverTime * Time.deltaTime);
+				fireTime -= Time.deltaTime;
+				//Enemy is no longer on fire
+				if (fireTime <= 0.0f)
+					state = E_STATE.NORMAL;
 				break;
 		}
 
-		//Cycle through all behaviours
-		acceleration = Vector3.zero;
-		if (engaging)
+		//Cycle through all behaviours or attack
+		if (engaging) //Attack
 		{
-			acceleration += engager.Update();
+			//Move directly towards our target
+			acceleration = target.transform.position - transform.position;
+
+			attackTimer -= Time.deltaTime;
+			if (attackLocked)
+			{
+				if (attackTimer <= 0.0f)
+				{
+					Attack();
+					//Allow movement and rotation again
+					attackLocked = false;
+					//Set the time for cooldown between attacks
+					attackTimer = attackRate;
+				}
+			}
 		}
-		else
+		else //Behaviours
 		{
+			acceleration = Vector3.zero;
 			foreach (BaseBehaviour b in behaviours)
 			{
 				acceleration += b.Update() * b.weight;
@@ -112,6 +138,13 @@ public class Enemy : MonoBehaviour
 		//Normalize it and multiply it by our speed
 		acceleration.Normalize();
 		acceleration *= movementSpeed;
+
+		//Prevent movement while attacking ------
+		if (attackLocked)
+		{
+			acceleration = Vector3.zero;
+			velocity = Vector3.zero;
+		}
 
 		velocity += acceleration / mass;
 		velocity *= drag;
@@ -127,7 +160,7 @@ public class Enemy : MonoBehaviour
 			velocity = Vector3.Normalize(velocity) * movementSpeed;
 		}
 
-		//Anchor the enemy to the floor (rigidbody fix)
+		//Anchor the enemy to the floor (rigidbody raycast fix)
 		transform.position = new Vector3(transform.position.x, 0.0f, transform.position.z);
 	}
 
@@ -166,10 +199,35 @@ public class Enemy : MonoBehaviour
 	}
 
 	//Ignite this enemy
-	public void Ignite(float _fire_strength = 10.0f)
+	public void Ignite(float _fire_strength = 10.0f, float _fire_duration = 2.0f)
 	{
 		state = E_STATE.IGNITED;
 		fireDamageOverTime = _fire_strength;
+	}
+
+	//Don't call this anywhere but AttackBox
+	//Signal to attempt an attack
+	public void AttackSignal()
+	{
+		//Only attack if we can
+		if (attackTimer <= 0.0f)
+		{
+			attackLocked = true;
+			attackTimer = attackDelay;
+			//DEBUG
+			//transform.Find("AttackWindup").GetComponent<ParticleSystem>().Play();
+		}
+	}
+	//Attack and deal damage if a player was hit
+	public void Attack()
+	{
+		//If there is no valid target in the attackbox then the attack is missed
+		if (attackHitbox.target != null)
+		{
+			attackHitbox.target.transform.GetComponent<PlayerHealth>().DamagePlayer(attackDamage);
+			//DEBUG
+			//transform.Find("AttackEffect").GetComponent<ParticleSystem>().Play();
+		}
 	}
 
 	private void OnDrawGizmos()
---
+++ Grave_Terror/Assets/Scripts/Dennis/EnemyAttackBox.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttackBox : MonoBehaviour
+{
+
+	Enemy parent;
+	public GameObject target = null;
+
+	void Awake ()
+	{
+		//Forceably rename the object i'm attached to too AttackBox
+		gameObject.name = "AttackBox";
+	}
+
+	void Start ()
+	{
+		parent = GetComponentInParent<Enemy>();
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("Player"))
+		{
+			target = other.gameObject;
+		}
+	}
+
+	private void OnTriggerStay(Collider other)
+	{
+		if (other.CompareTag("Player"))
+		{
+			parent.AttackSignal();
+		}
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		if (other.CompareTag("Player"))
+		{
+			target = null;
+		}
+	}
+}
