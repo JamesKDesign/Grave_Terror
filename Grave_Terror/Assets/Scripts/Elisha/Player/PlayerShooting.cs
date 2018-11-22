17cb9917f0dc70bb3a49e34b6fa84888a60d5c9b
JamesKDesign <39175119+JamesKDesign@users.noreply.github.com>
2018-11-22 11:14:41 +1100

    Added stamps, removed doubling in Scripts/Elisha/Player/PlayerShooting.cs

--- Grave_Terror/Assets/Scripts/Elisha/Player/PlayerShooting.cs
+++ Grave_Terror/Assets/Scripts/Elisha/Player/PlayerShooting.cs
@@ -38,7 +38,7 @@ public class PlayerShooting : MonoBehaviour
 
     void Shooting()
     {
-        if(health.playerState == PlayerHealth.PlayerState.ALIVE)
+        if (health.playerState == PlayerHealth.PlayerState.ALIVE)
         {
             if (xboxController.useController == true)
             {
@@ -51,88 +51,33 @@ public class PlayerShooting : MonoBehaviour
                 }
                 if (XCI.GetAxis(XboxAxis.RightTrigger, xboxController.controller) > 0.1f)
                 {
-					if (counter > delay)
-					{
-						//anim.SetBool("isShooting", true);
-						muzzleFlash.Play();
-						bulletCasing.Play();
-
-						// bullets
-						//anim.SetBool("IsAttacking", true);
-						bullet = Instantiate(particleProjectile, transform.position, chunkRotation.transform.rotation);
-						counter = 0f;
-
-						anim.SetBool("IsAttacking", true);
-
-						RaycastHit hit;
-						Ray rayCast = new Ray(transform.position, transform.forward);
-						if (Physics.Raycast(rayCast, out hit, range, layerMask))
-						{
-							// enemy health damaged
-							target = hit.transform.GetComponent<EnemyHealth>();
-							if (target != null)
-							{
-								// enemy health damaged
-								target = hit.transform.GetComponent<EnemyHealth>();
-								if (target != null)
-								{
-									target.DamageHealth(damageToGive);
-									// blood
-
-									cameraController.ShakeCamera();
-									Debug.Log("shake");
-
-									GameObject impact = Instantiate(bloodEffect, hit.point, Quaternion.FromToRotation(Vector3.up, hit.normal));
-									Destroy(impact, 0.5f);
-								}
-
-								DestructableObjects obj = hit.transform.GetComponent<DestructableObjects>();
-								if (obj != null)
-								{
-									obj.ObjectDamage(damageToGive);
-									// blood
-									holes = Instantiate(bulletHole, hit.point, Quaternion.FromToRotation(Vector3.up, hit.normal));
-									Destroy(holes, 0.2f);
-								}
-							}
-						}
-					}
-                }
-                else if (XCI.GetAxis(XboxAxis.RightTrigger, xboxController.controller) < 0.1)
-                {
-                    muzzleFlash.Stop();
-                    bulletCasing.Stop();
-
-                //anim.SetBool("IsAttacking", false);
-                }
-            }
-            else if (!xboxController.useController)
-            {
-                counter += Time.deltaTime;
-                if (Input.GetKeyDown(KeyCode.Mouse0) && counter > delay)
-                {
                     if (counter > delay)
                     {
                         //anim.SetBool("isShooting", true);
                         muzzleFlash.Play();
                         bulletCasing.Play();
+
                         // bullets
+                        //anim.SetBool("IsAttacking", true);
                         bullet = Instantiate(particleProjectile, transform.position, chunkRotation.transform.rotation);
-                        Destroy(bullet, 1f);
                         counter = 0f;
 
                         anim.SetBool("IsAttacking", true);
 
                         RaycastHit hit;
                         Ray rayCast = new Ray(transform.position, transform.forward);
-                        if (Physics.Raycast(rayCast, out hit, range))
+                        if (Physics.Raycast(rayCast, out hit, range, layerMask))
                         {
                             // enemy health damaged
-                            EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
+                            target = hit.transform.GetComponent<EnemyHealth>();
                             if (target != null)
                             {
                                 target.DamageHealth(damageToGive);
                                 // blood
+
+                                cameraController.ShakeCamera();
+                                Debug.Log("shake");
+
                                 GameObject impact = Instantiate(bloodEffect, hit.point, Quaternion.FromToRotation(Vector3.up, hit.normal));
                                 Destroy(impact, 0.5f);
                             }
@@ -145,20 +90,71 @@ public class PlayerShooting : MonoBehaviour
                                 holes = Instantiate(bulletHole, hit.point, Quaternion.FromToRotation(Vector3.up, hit.normal));
                                 Destroy(holes, 0.2f);
                             }
+
                         }
                     }
+                    else if (XCI.GetAxis(XboxAxis.RightTrigger, xboxController.controller) < 0.1)
+                    {
+                        muzzleFlash.Stop();
+                        bulletCasing.Stop();
+
+                        //anim.SetBool("IsAttacking", false);
+                    }
                 }
-                else if (Input.GetKeyUp(KeyCode.Mouse0) && counter < delay)
+                else if (!xboxController.useController)
                 {
-                    muzzleFlash.Stop();
-                    bulletCasing.Stop();
-                    anim.SetBool("IsAttacking", false);
+                    counter += Time.deltaTime;
+                    if (Input.GetKeyDown(KeyCode.Mouse0) && counter > delay)
+                    {
+                        if (counter > delay)
+                        {
+                            //anim.SetBool("isShooting", true);
+                            muzzleFlash.Play();
+                            bulletCasing.Play();
+                            // bullets
+                            bullet = Instantiate(particleProjectile, transform.position, chunkRotation.transform.rotation);
+                            Destroy(bullet, 1f);
+                            counter = 0f;
+
+                            anim.SetBool("IsAttacking", true);
+
+                            RaycastHit hit;
+                            Ray rayCast = new Ray(transform.position, transform.forward);
+                            if (Physics.Raycast(rayCast, out hit, range))
+                            {
+                                // enemy health damaged
+                                EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
+                                if (target != null)
+                                {
+                                    target.DamageHealth(damageToGive);
+                                    // blood
+                                    GameObject impact = Instantiate(bloodEffect, hit.point, Quaternion.FromToRotation(Vector3.up, hit.normal));
+                                    Destroy(impact, 0.5f);
+                                }
+
+                                DestructableObjects obj = hit.transform.GetComponent<DestructableObjects>();
+                                if (obj != null)
+                                {
+                                    obj.ObjectDamage(damageToGive);
+                                    // blood
+                                    holes = Instantiate(bulletHole, hit.point, Quaternion.FromToRotation(Vector3.up, hit.normal));
+                                    Destroy(holes, 0.2f);
+                                }
+                            }
+                        }
+                    }
+                    else if (Input.GetKeyUp(KeyCode.Mouse0) && counter < delay)
+                    {
+                        muzzleFlash.Stop();
+                        bulletCasing.Stop();
+                        anim.SetBool("IsAttacking", false);
+                    }
                 }
             }
-        }
-        else if(health.playerState == PlayerHealth.PlayerState.REVIVE && health.playerState == PlayerHealth.PlayerState.DEAD)
-        {
-            xboxController.useController = false;
+            else if (health.playerState == PlayerHealth.PlayerState.REVIVE && health.playerState == PlayerHealth.PlayerState.DEAD)
+            {
+                xboxController.useController = false;
+            }
         }
     }
 }
