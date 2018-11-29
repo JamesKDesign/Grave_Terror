6c0fe9c2378cbda28d3352fe201dd986ee22524e
Fishingpilot <warmbaconsalad@gmail.com>
2018-11-29 12:25:42 +1100

    added some other trigger james really loved

---
+++ Grave_Terror/Assets/Scripts/Dennis/TriggerDestroyOther.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerDestroyOther : MonoBehaviour
+{
+	public GameObject[] waitForDestruction;
+
+	public UnityEvent triggers;
+	private void Update()
+	{
+		foreach (GameObject o in waitForDestruction)
+		{
+			if (o == null)
+			{
+				continue;
+			}
+			return;
+		}
+		triggers.Invoke();
+		//jobs done
+		Destroy(this);
+	}
+}
